7e5c49659fb4ac40b024f7d7c4f38b0dbb06f1ba
Suhaib Ersan <>
2021-04-26 06:09:59 +0000

    x

--- scripts.js
+++ scripts.js
@@ -12,3 +12,5 @@ console.log('User entered the name = ' + nameVar);
 console.log('User\'s age = ' + ageVar);
 console.log('User prefers the color = ' + preferredColor);
 console.log('User prefers the season = ' + preferredSeason);
+
+document.write('Hello, ' + nameVar + ' Who\'s ' + ageVar + ' years old, ' + preferredColor + ' is a beautiful color, I also like ' + preferredSeason + '.');
